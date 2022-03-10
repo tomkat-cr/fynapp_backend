b7911d23cf1ea36822579623459defb1d2152ec6
Carlos J. Ramirez <tomkat_cr@yahoo.com>
2022-03-10 04:53:58 +0000

    Implement FA-13, FA-14, FA-15, FA-16, FA-17, FA-18, FA-21, FA-22, FA-3, FA-23

--- README.md
+++ README.md
@@ -1,44 +1,45 @@
-# Platzi Mongo
-Todo el código que escribirás para el proyecto platzi-mongo
-estará en el archivo db.py
+# FynApp Backend
+FynApp es una aplicación de gestión para lograr objetivos de alimentación adecuada y mantener un mejor estilo de vida basada en constante actividad física.
+2021-10-16 | CR
 
-# Postman
-1. Descargar e instalar [Postman](https://www.getpostman.com/downloads/)
-2. La URI de las colecciones de Postman usada para el proyecto está en [Postman-platzi-mongo](https://www.getpostman.com/collections/ffcbfb5c8d5cd2dc52d2)
-3. Importar colección dentro de Postman [Instrucciones](https://learning.getpostman.com/docs/postman/collections/data_formats/#exporting-and-importing-postman-data)
-
-## Instalar Anaconda 
-La forma más simple de ejecutar el proyecto es instalando [Anaconda](https://www.anaconda.com/distribution/).
-
-Con Anaconda instalado de manera correcta, navegar hasta el directorio del proyecto
-y ejecutar: 
+## Iniciar el sistema
 ```
-# navegar hasta el directorio del proyecto
-cd platzi-mongo
-# crear un nuevo ambiente
-conda create --name platzi-mongo
-# activar el ambiente 
-conda activate platzi-mongo
-# para desactivar el ambiente
-conda deactivate
+export DEV_BASE_DIR="your_base_development_directory_path"
+source ${DEV_BASE_DIR}/fynapp_backend/run_fynapp_backend.sh
 ```
-Si usas Python frecuentemente y tienes una versión 3.3+ no es necesario que 
-instales Anaconda, crea un ambiente virtual con venv o virtualenv y sigue con 
-el paso de instalar las dependencias.
+
+## Ejecutar los test unitatios
+```
+source ${DEV_BASE_DIR}/fynapp_backend/run_fynapp_backend.sh test
+```
+
 ## Instalar dependenias del proyecto
 Con el ambiente activado, instalar las dependencias:
 ```
 pip install -r requirements.txt
 ```
+
 ## Variables de entorno necesarias para ejecutar el proyecto
-Asegurate de reemplazar el valor de PLATZI_DB_URI por la URI de tu cluster en MongoDB Atlas
+Asegurate de reemplazar el valor de FYNAPP_DB_URI por la URI de tu cluster en MongoDB Atlas
+Para los ambientes de produccion, estas variables se deben asignar en un archivo .env
 ```
-export FLASK_APP=platzi-api
-export FLASK_ENV=development 
-export PLATZI_DB_URI="MONGO-URI"
+export FYNAPP_SECRET_KEY="secret"
+export FLASK_APP="fynapp_api"
+export FLASK_ENV="development"
+export FYNAPP_DB_ENV="dev"
+export FYNAPP_DB_SERVER="xxxxxxx"
+export FYNAPP_DB_NAME="fynapp_${FYNAPP_DB_ENV}"
+export FYNAPP_DB_URI="mongodb+srv://${FYNAPP_DB_USR_NAME}:${FYNAPP_DB_USR_PSW}@${FYNAPP_DB_SERVER}"
 ```
 
-## Iniciar el servidor de platzi-mongo
+## Iniciar el servidor de fynapp_backend
 ```
 flask run
 ```
+# Postman
+1. Descargar e instalar [Postman](https://www.getpostman.com/downloads/)
+2. La URI de las colecciones de Postman usada para el proyecto está en [fynapp_backend/FynApp.postman_collection.json](https://gitlab.com/tomkat-cr/fynapp_backend/-/raw/main/postman_collection.json)
+3. Importar colección dentro de Postman [Instrucciones](https://learning.getpostman.com/docs/postman/collections/data_formats/#exporting-and-importing-postman-data)
+
+# NOTA:
+Normalmente el backend usa el puerto 5000.
--- fynapp_api/models/db.py
+++ fynapp_api/models/db.py
@@ -3,8 +3,16 @@ from flask import current_app
 from pymongo import MongoClient, DESCENDING
 from werkzeug.local import LocalProxy
 
-import hashlib
+from werkzeug.security import generate_password_hash, check_password_hash
 
+from flask import request
+#import uuid
+import jwt
+import datetime
+from functools import wraps
+
+from fynapp_api.util.app_logger import log_debug, log_warning
+from fynapp_api.util.utilities import check_email, standard_error_return, get_standard_base_exception_msg, current_datetime_timestamp, get_default_resultset
 
 # Este método se encarga de configurar la conexión con la base de datos
 def get_db():
@@ -18,6 +26,8 @@ def get_db():
 # Use LocalProxy to read the global db instance with just `db`
 db = LocalProxy(get_db)
 
+header_token_entry_name = 'x-access-tokens'
+
 
 def test_connection():
     return dumps(db.list_collection_names())
@@ -25,6 +35,44 @@ def test_connection():
 
 def collection_stats(collection_nombre):
     return dumps(db.command('collstats', collection_nombre))
+
+
+# ----------------------- jwt -----------------------
+
+
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None
+        # log_debug( 'token_required | request.headers: {}'.format(request.headers) )
+        if header_token_entry_name in request.headers:
+            token = request.headers[header_token_entry_name]
+        if not token:
+            return standard_error_return('a valid token is missing')
+        # log_debug( 'la clave: {}'.format(current_app.config['FYNAPP_SECRET_KEY']) )
+        # log_debug( 'el token: {}'.format(token) )
+        try:
+            data = jwt.decode(token, current_app.config['FYNAPP_SECRET_KEY'], algorithms="HS256")
+            # current_user = fetch_user_raw(users_id=data['public_id'])
+        except:
+            return standard_error_return('token is invalid')
+        # return f(current_user, *args, **kwargs)
+        return f(*args, **kwargs)
+    return decorator
+
+
+def token_encode(user):
+    token = jwt.encode(
+        {
+            'public_id': get_user_id_as_string(user),
+            'exp' : 
+                datetime.datetime.utcnow() + 
+                datetime.timedelta(minutes=30)
+        },
+        current_app.config['FYNAPP_SECRET_KEY'],
+        algorithm="HS256"
+    )
+    return token
 
 # ...
 # ...
@@ -36,16 +84,24 @@ def collection_stats(collection_nombre):
 # PK: _id
 # Fields:
 #     _id
-#     passcode
-#     firsname
+#     firstname
 #     lastname
-#     creation_date
-#     birthday
+#     superuser
 #     email
+#     passcode
+#     birthday
 #     height
 #     height_unit
+#     weight
+#     weight_unit
+#     tall
+#     tall_unit
 #     training_days
 #     training_hour
+#     eating_hours
+#     creation_date
+#     update_date
+
 #     user_history: 
 #       [
 #           {
@@ -63,80 +119,257 @@ def collection_stats(collection_nombre):
 #           }
 #       ]
 
+    # try:
+    # except BaseException as err:
+    #     resultset['error_message'] = get_standard_base_exception_msg(err, 'XX')
+    #     resultset['error'] = True
+
 
 def fetch_users_list(skip, limit):
+    resultset = get_default_resultset()
+
     proyeccion = {'passcode': 0}
-    return dumps(db.users.find({}, proyeccion).skip(int(skip)).limit(int(limit)))
+    try:
+        resultset['resultset'] = dumps(db.users.find({}, proyeccion).skip(int(skip)).limit(int(limit)))
+    except BaseException as err:
+        resultset['error_message'] = get_standard_base_exception_msg(err, 'FUL1')
+        resultset['error'] = True
+
+    return resultset
 
 
 def fetch_user(users_id):
-    return dumps(db.users.find_one({'_id': ObjectId(users_id)}))
+    resultset = get_default_resultset()
+
+    existing_user = fetch_user_raw(users_id)
+    if not existing_user['resultset']:
+        resultset['error_message'] = 'UserId {} doesn\'t exist [FU1].'.format(users_id)
+    elif existing_user['error']:
+        resultset['error_message'] = existing_user['error_message']
+
+    if resultset['error_message']:
+        resultset['error'] = True
+        return resultset
+
+    try:
+        resultset['resultset'] = dumps(existing_user['resultset'])
+    except BaseException as err:
+        resultset['error_message'] = get_standard_base_exception_msg(err, 'FU2')
+        resultset['error'] = True
+
+    return resultset
+
+
+def fetch_user_raw(users_id):
+    resultset = get_default_resultset()
+
+    try:
+        id = ObjectId(users_id)
+    except ValueError:
+        resultset['error_message'] = 'UserId `{}` is invalid [FUR1].'.format(users_id)
+    except BaseException as err:
+        resultset['error_message'] = get_standard_base_exception_msg(err, 'FUR2')
+        # raise
+
+    if resultset['error_message']:
+        resultset['error'] = True
+        return resultset
+
+    try:
+        resultset['resultset'] = db.users.find_one({'_id': id})
+    except BaseException as err:
+        resultset['error_message'] = get_standard_base_exception_msg(err, 'FUR3')
+        resultset['error'] = True
+
+    return resultset
+
+
+def fetch_user_by_entryname_raw(entry_name, entry_value):
+    resultset = get_default_resultset()
+
+    try:
+        resultset['resultset'] = db.users.find_one({entry_name: entry_value})
+    except BaseException as err:
+        resultset['error_message'] = get_standard_base_exception_msg(err, 'FUBEN1')
+        resultset['error'] = True
+
+    return resultset
+
+
+def get_user_id_as_string(user):
+    # log_debug( 'get_user_id_as_string) user[]: {}'.format(user) )
+    return str(user['_id'])
 
 
 def create_user(json):
+    resultset = get_default_resultset()
+
+    if not 'email' in json:
+        resultset['error_message'] = 'error: Email wasn\'t specified [CU1].'
+    elif json['email'] == 'foo@baz.com' and not ('pytest_run' in json and json['pytest_run'] == 1):
+            resultset['error_message'] = 'error: User {} is invalid [CU2].'.format(json['email'])
+    elif not check_email(json['email']):
+        resultset['error_message'] = 'error: Malformed email {} [CU3].'.format(json['email'])
+    else:
+        existing_user = fetch_user_by_entryname_raw('email', json['email'])
+        if existing_user['resultset']:
+            resultset['error_message'] = 'User {} already exists [CU4].'.format(json['email'])
+        elif existing_user['error']:
+            resultset['error_message'] = existing_user['error_message']
+
+    if resultset['error_message']:
+        resultset['error'] = True
+        return resultset
+
     if ('passcode' in json):
         json['passcode'] = encrypt_password(json['passcode'])
-    # if (not 'food_times' in json):
-    #     json['food_times'] = []
-    # if (not 'user_history' in json):
-    #     json['user_history'] = []
-    return str(db.users.insert_one(json).inserted_id)
+    json['creation_date'] = json['update_date'] = current_datetime_timestamp()
+
+    try:
+        resultset['resultset']['_id'] = str(db.users.insert_one(json).inserted_id)
+    except BaseException as err:
+        resultset['error_message'] = get_standard_base_exception_msg(err, 'CU5')
+        resultset['error'] = True
+    else:
+        resultset['resultset']['rows_affected'] = '1'
+
+    return resultset
 
 
 def update_users(record):
-    updated_record = {
-        'firsname': record['firsname'],
-        'lastname': record['lastname'],
-        'creation_date': record['creation_date'],
-        'birthday': record['birthday'],
-        'email': record['email'],
-        'height': record['height'],
-        'height_unit': record['height_unit'],
-        'training_days': record['training_days'],
-        'training_hour': record['training_hour'],
+    resultset = get_default_resultset()
+
+    mandatory_elements = {
+        'firstname',
+        'lastname',
+        'creation_date',
+        'birthday',
+        'email',
+        'height',
+        'height_unit',
+        'weight',
+        'weight_unit',
+        'training_days',
+        'training_hour',
     }
-    if ('passcode' in record):
+    updated_record = dict(record)
+    resultset['error_message'] = ''
+    for element in mandatory_elements:
+        if(element not in record):
+            resultset['error_message'] = '{}{} {}'.format(
+                resultset['error_message'],
+                ', ' if resultset['error_message'] != '' else '',
+                element
+            )
+
+    if resultset['error_message']:
+        resultset['error_message'] = 'Missing mandatory elements: {} [UU1].'.format(resultset['error_message'])
+        resultset['error'] = True
+        return resultset
+
+    updated_record['update_date'] = current_datetime_timestamp()
+    if ('passcode' in record and record['passcode']):
         updated_record['passcode'] = encrypt_password(record['passcode'])
-    return str(db.users.update_one({'_id': ObjectId(record['_id'])}, {
-        '$set': updated_record
-    }).modified_count)
 
+    if '_id' not in record and 'id' in record:
+        record['_id'] = record['id']
+        del record['id']
 
-def encrypt_password(passcode):
-    return hashlib.md5(passcode.encode('utf8')).hexdigest()
+    try:
+        resultset['resultset']['rows_affected'] = str(db.users.update_one({'_id': ObjectId(record['_id'])}, {
+            '$set': updated_record
+        }).modified_count)
+    except BaseException as err:
+        resultset['error_message'] = get_standard_base_exception_msg(err, 'UU2')
+        resultset['error'] = True
+
+    return resultset
 
 
 def delete_user(user_id):
-    return str(db.users.delete_one({'_id': ObjectId(user_id)}).deleted_count)
+    resultset = get_default_resultset()
+
+    existing_user = fetch_user_by_entryname_raw('_id', ObjectId(user_id))
+    if not existing_user['resultset']:
+        resultset['error_message'] = 'error: User {} doesn\'t exist [DU1].'.format(user_id)
+    elif existing_user['error']:
+        resultset['error_message'] = existing_user['error_message']
+
+    if resultset['error_message']:
+        resultset['error'] = True
+        return resultset
+
+    try:
+        resultset['resultset']['rows_affected'] = str(db.users.delete_one({'_id': ObjectId(user_id)}).deleted_count)
+    except BaseException as err:
+        resultset['error_message'] = get_standard_base_exception_msg(err, 'DU2')
+        resultset['error'] = True
+
+    return resultset
+
+
+# ----- passwords
+
+
+def encrypt_password(passcode):
+    return generate_password_hash(passcode, method='sha256')
+
+
+def verify_password(user_password, auth_password):
+    return check_password_hash(user_password, auth_password)
 
 
 # ----- food_times
 
 
 def add_food_times_to_user(json):
-    return str(db.users.update_one({'_id': ObjectId(json['user_id'])}, {
-        '$addToSet': {'food_times': json['food_times']}}).modified_count)
+    resultset = get_default_resultset()
+    try:
+        resultset['resultset']['rows_affected'] = str(db.users.update_one({'_id': ObjectId(json['user_id'])}, {
+            '$addToSet': {'food_times': json['food_times']}}).modified_count)
+    except BaseException as err:
+        resultset['error_message'] = get_standard_base_exception_msg(err, 'AFTTU1')
+        resultset['error'] = True
+    return resultset
 
 
 def remove_food_times_to_user(json):
-    return str(db.users.update_one({'_id': ObjectId(json['user_id'])}, {
-        '$pull': {'food_times': {'food_moment_id': json['food_moment_id']}}
-    }).modified_count)
+    resultset = get_default_resultset()
+    try:
+        resultset['resultset']['rows_affected'] = str(db.users.update_one({'_id': ObjectId(json['user_id'])}, {
+            '$pull': {'food_times': {'food_moment_id': json['food_moment_id']}}
+        }).modified_count)
+    except BaseException as err:
+        resultset['error_message'] = get_standard_base_exception_msg(err, 'RFTTU')
+        resultset['error'] = True
+    return resultset
 
 
 # ----- user_history
 
 
 def add_user_history_to_user(json):
-    # curso = consultar_curso_por_id_proyeccion(json['id_curso'], proyeccion={'nombre': 1})
-    return str(db.users.update_one({'_id': ObjectId(json['user_id'])}, {
-        '$addToSet': {'user_history': json['user_history']}}).modified_count)
+    resultset = get_default_resultset()
+    try:
+        # curso = consultar_curso_por_id_proyeccion(json['id_curso'], proyeccion={'nombre': 1})
+        resultset['resultset']['rows_affected'] = str(db.users.update_one({'_id': ObjectId(json['user_id'])}, {
+            '$addToSet': {'user_history': json['user_history']}}).modified_count)
+    except BaseException as err:
+        resultset['error_message'] = get_standard_base_exception_msg(err, 'AUHTU1')
+        resultset['error'] = True
+    return resultset
 
 
 def remove_user_history_to_user(json):
-    return str(db.users.update_one({'_id': ObjectId(json['user_id'])}, {
-        '$pull': {'user_history': {'date': json['date']}}
-    }).modified_count)
+    resultset = get_default_resultset()
+    try:
+        resultset['resultset']['rows_affected'] = str(db.users.update_one({'_id': ObjectId(json['user_id'])}, {
+            '$pull': {'user_history': {'date': json['date']}}
+        }).modified_count)
+    except BaseException as err:
+        resultset['error_message'] = get_standard_base_exception_msg(err, 'RUHTU')
+        resultset['error'] = True
+    return resultset
 
 
 # ----------------------- security_groups -----------------------
