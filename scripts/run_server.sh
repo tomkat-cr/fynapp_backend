3aa57a3fd944327fdeca372d94690bcaf746ec83
Carlos J. Ramirez <tomkat_cr@yahoo.com>
2022-09-05 21:32:07 -0500

    Add: run_server.sh w/params to deploy on fleek.co

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -9,7 +9,18 @@ python3 -m venv venv
 . venv/bin/activate
 pip3 install -r requirements.txt
 
-gunicorn -b 0.0.0.0:5001 --log-level debug -w 4 'fynapp_api.__init__:create_app()'
+PORT=$1
+BASE_DIR=$2
+if [ "$PORT" = "" ]; then
+    PORT="5001"
+fi
+if [ "$BASE_DIR" = "" ]; then
+    BASE_DIR="."
+fi
+mkdir -p ${BASE_DIR}/logs ;
+touch ${BASE_DIR}/logs/fynapp_general.log
+
+gunicorn -b 0.0.0.0:${PORT} --log-level debug -w 4 'fynapp_api.__init__:create_app()'
 
 # Ports in use
 # sudo lsof -PiTCP -sTCP:LISTEN
